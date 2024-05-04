d03cdebe39333bed842aae61b2dfb7d4bd9ca621
PRRInc <prrincdev@gmail.com>
2024-05-04 18:08:25 -0400

    Update Home Component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,3 +1,73 @@
+import { Link } from "react-router-dom";
+import { getAllNotes } from "../../api/fetch";
+import ErrorMessage from "../errors/ErrorMessage";
+import { useEffect, useState } from "react";
+import ShowListing from "./ShowListing";
+
+import "./ShowsIndex.css";
+
+
+
 export default function Home() {
-    return null;
+  const [loadingError, setLoadingError] = useState(false);
+  const [notes, setNotes] = useState([]);
+  const [allNotes, setAllNotes] = useState([]);
+  const [searchTitle, setSearchTitle] = useState("");
+
+  function handleTextChange(event) {
+    const title = event.target.value;
+    const result = title.length ? filterShows(title, allShows) : allShows;
+  
+    setSearchTitle(title);
+    setShows(result);
+  }
+
+  function filterShows(search, shows) {
+    return shows.filter((show) => {
+      return show.title.toLowerCase().match(search.toLowerCase());
+    });
+  }
+
+  useEffect(() => {
+    getAllNotes()
+      .then((response) => {
+        setAllShows(response);
+        setShows(response);
+        setLoadingError(false);
+      })
+      .catch((error) => {
+        console.error(error);
+        setLoadingError(true);
+      });
+  }, []);
+
+  return (
+    <div>
+      {loadingError ? (
+        <ErrorMessage />
+      ) : (
+        <section className="shows-index-wrapper">
+          <h2>All Shows</h2>
+          <button>
+            <Link to="/shows/new">Add a new show</Link>
+          </button>
+          <br />
+          <label htmlFor="searchTitle">
+            Search Shows:
+            <input
+              type="text"
+              value={searchTitle}
+              id="searchTitle"
+              onChange={handleTextChange}
+            />
+          </label>
+          <section className="shows-index">
+            {shows.map((show) => {
+              return <ShowListing show={show} key={show.id} />;
+            })}
+          </section>
+        </section>
+      )}
+    </div>
+  );
 }
