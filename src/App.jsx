820e9a32bf706fdd9f90f79785ca76f61bc20af4
PRRInc <prrincdev@gmail.com>
2024-05-04 14:32:48 -0400

    Update App.jsx with new Routes and Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
+
+// Components
+import Footer from "./components/common/Footer";
+import Home from "./components/home/Home";
+import NavBar from "./components/NavBar";
+import Note from "./components/Notes/Note";
+import EditNote from "./components/Notes/EditNote";
+import NewNote from "./components/Notes/NewNote";
+
 
 function App() {
 
@@ -12,6 +19,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/aboutus" element={<AboutUs />} />
+          <Route path="/notes/new" element={<NewNote />} />
+          <Route path="/notes/:id" element={<Note />} />
+          <Route path="/notes/:id/edit" element={<EditNote />} />
         </Routes>
         <Footer />
       </Router>
