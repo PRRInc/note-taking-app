ec44daa01ba3ef43d527d8c6573cdeb3db38431d
Bryan Alcantara <alcantarabryan0418@gmail.com>
2024-05-05 15:04:59 -0400

    added the new note page component

--- src/components/Notes/NewNote.jsx
+++ src/components/Notes/NewNote.jsx
@@ -1,7 +1,114 @@
-export default function NewNote() {
-    return (
-        <>
-            <p>New Note</p>
-        </>
+import "./NewNote.css"
+
+export default function NewNote(){
+
+        // const [note, setNote] = useState({
+        //     category: "",
+        //     title: "",
+        //     country: "",
+        //     dateAdded: "",
+        //     description: "",
+        //     tags: "",
+        //     priority: "",
+        //   });
+        
+        //   let navigate = useNavigate();
+        
+        //   function handleTextChange(event) {
+        //     setNote({
+        //       ...show,
+        //       [event.target.id]: event.target.value,
+        //     });
+        //   }
+        
+        //   function handleSubmit(event) {
+        //     event.preventDefault();
+        //     createNote(note)
+        //       .then((response) => {
+        //         navigate(`/shows/${response.id}`);
+        //       })
+        //       .catch((error) => {
+        //         console.error(error);
+        //       });
+        //   }
+        
+          return (
+            <form >
+              <label htmlFor="title">Title:</label>
+              <input
+                type="text"
+                id="title"
+                // value={show.title}
+                // onChange={handleTextChange}
+              />
+        
+              <label htmlFor="description">Description:</label>
+              <input
+                type="text"
+                id="description"
+                // value={show.description}
+                // onChange={handleTextChange}
+              />
+        
+              <label htmlFor="type">Type</label>
+              <input
+                type="text"
+                id="type"
+                // value={show.type}
+                // onChange={handleTextChange}
+              />
+        
+              <label htmlFor="rating">Rating:</label>
+              <input
+                type="text"
+                id="rating"
+                // value={show.rating}
+                // onChange={handleTextChange}
+              />
+        
+              <label htmlFor="listedIn">Listed in</label>
+              <input
+                type="text"
+                id="listedIn"
+                // value={show.listedIn}
+                // onChange={handleTextChange}
+              />
+        
+              <label htmlFor="duration">Duration</label>
+              <input
+                type="text"
+                id="duration"
+                // value={show.duration}
+                // onChange={handleTextChange}
+              />
+        
+              <label htmlFor="releaseYear">Release Year</label>
+              <input
+                type="text"
+                id="releaseYear"
+                // value={show.releaseYear}
+                // onChange={handleTextChange}
+              />
+        
+              <label htmlFor="country">Country</label>
+              <input
+                type="text"
+                id="country"
+                // value={show.country}
+                // onChange={handleTextChange}
+              />
+        
+              <label htmlFor="dateAdded">Date added:</label>
+              <input
+                type="text"
+                id="dateAdded"
+                // value={show.dateAdded}
+                // onChange={handleTextChange}
+              />
+        
+              <br />
+        
+              <input type="submit" />
+            </form>
     )
 }
