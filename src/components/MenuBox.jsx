33135650daf6b933dc87df585866309c41b70f91
JorgeLG87 <jorge85_6@hotmail.com>
2024-05-05 14:12:45 -0400

    Searchbar and navbar formatting update

--- src/components/MenuBox.jsx
+++ src/components/MenuBox.jsx
@@ -1,14 +1,24 @@
 import "./MenuBox.css";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
-export default function MenuBar() {
+
+
+export default function MenuBar({ handleSearchBar }) {
+
+    
+
     return (
         <div className="menubox">
             <ul className="list-container">
-                <li className="list-item-add">Add</li>
-                <li className="list-item-search">Search</li>
+                <Link style={{ "text-decoration": "none" }} to="/notes/new">
+                    <li className="list-item-add">Add</li>
+                </Link>
+                <li onClick={handleSearchBar} className="list-item-search">Search</li>
                 <li className="list-item-tags">Tags</li>
                 <li className="list-item-aboutus">About Us</li>
             </ul>
+            
         </div>
     )
 }
